3ea0ffbd08aed167b3dbb9a8bc9bafb92b7b0cb5
Martwy4 <martwy4@gmail.com>
2026-03-07 14:37:14 +0100

    Add files via upload

--- blog/routes.py
+++ blog/routes.py
@@ -3,8 +3,8 @@ from blog.models import Post
 
 bp = Blueprint("main", __name__)
 
-
 @bp.route("/")
 def homepage():
     posts = Post.query.all()
     return render_template("index.html", posts=posts)
+    
